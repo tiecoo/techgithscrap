233dab8e8c9ef341b90025e25a8c24aa8e09f55e
Diego <diego.pereirasilva@live.com>
2016-10-11 00:22:16 -0300

    que loucura

--- fetchceviu.rb
+++ fetchceviu.rb
@@ -6,3 +6,8 @@ link = page.link_with(class: 'tituloVaga')
 page = link.click
 puts page.uri
 puts page.title
+page = mechanize.get('http://www.ceviu.com.br/buscar/empregos?level=1&novaPesquisa=1&ini=0&termoPesquisa=&salarioMin=0&salarioMax=99999&salarioCombinar=1&diasMin=0&diasMax=90&ordenar=0&itensPagina=100')
+
+page.search('.tituloVaga').each do |titulo|
+  puts titulo.text.strip
+end
