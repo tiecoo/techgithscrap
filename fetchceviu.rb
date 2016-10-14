c1aac43088dfd91f4803749da80233befa2938f2
Diego <diego.pereirasilva@live.com>
2016-10-14 19:07:04 -0300

    funcionando usando proxy

--- fetchceviu.rb
+++ fetchceviu.rb
@@ -5,6 +5,8 @@ require 'nokogiri'
 require 'yaml'
 require 'slowweb'
 mechanize = Mechanize.new
+mechanize.set_proxy("223.67.136.218", 80)
+
 mechanize.user_agent_alias = 'Windows Chrome'
 @i = 0
 total_vagas = []
@@ -23,7 +25,6 @@ while @page.at('.proxima a') do
          vaga_infos[:moreinfo] = page.at('ul.list-unstyled.mais-informacoes') ? page.at('ul.list-unstyled.mais-informacoes').text : ""
          puts @i
          @i = 1 + @i
-         sleep(60)
      end
      @init = 100 + @init
      @url = 'http://www.ceviu.com.br/buscar/empregos?level=1&novaPesquisa=1&ini='+ @init.to_s  + '&termoPesquisa=&salarioMin=0&salarioMax=99999&salarioCombinar=1&diasMin=0&diasMax=90&ordenar=0&itensPagina=100'
